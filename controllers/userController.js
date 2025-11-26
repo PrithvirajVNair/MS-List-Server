93b019fb95ca2912a1fa79486bc57864c7fd5d25
PrithvirajVNair <prithvirajvnair.pvr@gmail.com>
2025-11-26 20:43:59 +0530

    OAUTH

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,55 +2,82 @@
 const users = require("../models/userModel")
 const bcrypt = require("bcrypt")
 const saltRounds = 10
+const jwt = require('jsonwebtoken')
+
 //register
-exports.registerController = async(req,res)=>{
-    const {username,email,password} = req.body
-    try{
-        const existingUser = await users.findOne({$or:[{email},{username}]})
-        if(existingUser){
-            if(existingUser.email == email){
+exports.registerController = async (req, res) => {
+    const { username, email, password } = req.body
+    try {
+        const existingUser = await users.findOne({ $or: [{ email }, { username }] })
+        if (existingUser) {
+            if (existingUser.email == email) {
                 return res.status(400).json("Already Registered User...")
             }
-            if(existingUser.username == username){
+            if (existingUser.username == username) {
                 return res.status(400).json("Username already taken!")
             }
         }
-        else{
+        else {
             const hashedPassword = await bcrypt.hash(password, saltRounds)
             const newUser = new users({
-                username,email,password:hashedPassword
+                username, email, password: hashedPassword
             })
             await newUser.save()
             res.status(200).json(newUser)
         }
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err)
         console.log(err);
-        
+
     }
 }
 
 //login
-exports.loginController = async(req,res) => {
-    const {username,password} = req.body
-    try{
-        const existingUser = await users.findOne({username})
-        if(existingUser){
-            const match = await bcrypt.compare(password,existingUser.password)
-            if(match){
+exports.loginController = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const existingUser = await users.findOne({ email })
+        if (existingUser) {
+            const match = await bcrypt.compare(password, existingUser.password)
+            if (match) {
                 return res.status(200).json(existingUser)
             }
-            else{
+            else {
                 return res.status(401).json("Password Does not Match!")
             }
         }
-        else{
+        else {
             res.status(401).json("User Does Not Exist..")
         }
-    }catch(err){
+    } catch (err) {
         res.status(500).json(err)
         console.log(err);
-        
+
+    }
+}
+
+//login
+exports.googleLoginController = async (req, res) => {
+    const { email, password, username, photo } = req.body
+    hashedPassword = await bcrypt.hash(password,saltRounds)
+    try {
+        const existingUser = await users.findOne({ email })
+        if (existingUser) {
+            const token = jwt.sign({ userMail: existingUser.email }, "secretkey")
+            res.status(200).json({ existingUser, token })
+        }
+        else {
+            const newUser = new users({
+                username, email, password:hashedPassword, profile:photo
+            })
+            await newUser.save()
+            const token = jwt.sign({ userMail: newUser.email }, "secretkey")
+            res.status(200).json({existingUser:newUser,token})
+        }
+    } catch (err) {
+        res.status(500).json(err)
+        console.log(err);
+
     }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -13,4 +13,7 @@ route.post("/register",userController.registerController)
 //path to login
 route.post("/login",userController.loginController)
 
+//path to google login
+route.post("/google-login",userController.googleLoginController)
+
 module.exports = route
